af03b0568c6e27bcfc1ecbf6bb1e45a28f0426f9
mvblanco99 <manuelvalentin1999@gmail.com>
2024-04-29 16:02:01 -0400

    Quitamos linea console.log

--- src/utils/readErrors.ts
+++ src/utils/readErrors.ts
@@ -1,5 +1,4 @@
 export const readError = (error) => {
-    console.log(error)
     if(error.code === 'ERR_CANCELED') console.log(error.message);
     if(error.code === 'ECONNABORTED') return { status: 500, statusText:'El servidor ha tardado mucho en responder'}; 
     if(error?.request.status === 422) return {status: 422, errors: error?.response?.data?.errors};
