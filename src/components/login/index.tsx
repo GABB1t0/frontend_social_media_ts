665828bd6b1da75f347dd38504f1d0480db34469
mvblanco99 <manuelvalentin1999@gmail.com>
2024-04-29 12:42:11 -0400

    Agregando manejo y mensajes de errores

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -16,11 +16,10 @@ import { actionsForErrors } from "../../utils/actionsForErrors";
 const TemplateLogin = () => {
 
     const { searchingToken } = useVerifySession();
-    const [error, setError] = useState();
+    const [error, setError] = useState<string[]>([]);
     const apiClient = client();
     const { navigate } = useRouter()
     
-  
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         const formData = new FormData(e.currentTarget);
@@ -33,43 +32,53 @@ const TemplateLogin = () => {
             navigate('/');
         })
         .catch(err => {
-            console.log(err)
             if(err?.status === 422){
-                setError(err.errors);
+                if(error.length > 0) setError([])
+                const errors = err?.errors?.email[0];
+                setError([...error,errors]);
                 return;
             }
             const {status, statusText} = error;
             actionsForErrors({status: status, statusText:statusText})
         });
-
     }
+
     return(
         <>
-        { searchingToken &&
-            <div className="h-screen w-full flex justify-center bg-gray-100 ">
-            <div className="bg-white w-72 h-96 flex flex-col  mx-auto my-auto justify-center gap-4">
-                <p className="text-2xl font-bold text-center">Iniciar Sesion</p>
-                <form onSubmit={handleSubmit} className="flex flex-col justify-center w-4/5 mx-auto gap-4">
-                <div className="flex flex-col">
-                    <label htmlFor="email">Correo Electronico</label>
-                    <InputSession 
-                        name="email" 
-                        type="email" 
-                        placeholder="Ingrese Correo" 
-                        required={true} />
-                    <label htmlFor="password">Contraseña</label>
-                    <InputSession 
-                        name="password" 
-                        type="password" 
-                        placeholder="Ingrese Contraseña" 
-                        required={true} />
+            { searchingToken &&
+                <div>
+                    <div className="h-screen w-full flex flex-col justify-center items-center gap-4 bg-gray-100">
+                        {
+                            error.length > 0 && 
+                                <div className="flex justify-center items-center w-72 h-14 text-sm text-red-500 bg-white border-2 border-gray-200">
+                                    <p>{error[0]}</p>
+                                </div>
+                        }
+                        
+                        <div className="bg-white w-72 h-96 flex flex-col justify-center gap-4">
+                            <p className="text-2xl font-bold text-center">Iniciar Sesion</p>
+                            <form onSubmit={handleSubmit} className="flex flex-col justify-center w-4/5 mx-auto gap-4">
+                                <div className="flex flex-col">
+                                    <label htmlFor="email">Correo Electronico</label>
+                                    <InputSession 
+                                        name="email" 
+                                        type="email" 
+                                        placeholder="Ingrese Correo" 
+                                        required={true} />
+                                    <label htmlFor="password">Contraseña</label>
+                                    <InputSession 
+                                        name="password" 
+                                        type="password" 
+                                        placeholder="Ingrese Contraseña" 
+                                        required={true} />
+                                </div>
+                                <button type="submit" className="bg-[#fc6232] hover:bg-orange-700 text-white font-bold py-2 px-4 rounded" >Iniciar Sesion</button>
+                                <p className="text-center">No tienes cuenta? <Link to={SUPPORTED_ROUTES.signUp()} className="text-[#fc6232]">Registrate</Link></p>
+                            </form>
+                        </div>
+                    </div>
                 </div>
-                <button type="submit" className="bg-[#fc6232] hover:bg-orange-700 text-white font-bold py-2 px-4 rounded" >Iniciar Sesion</button>
-                <p className="text-center">No tienes cuenta? <Link to={SUPPORTED_ROUTES.signUp()} className="text-[#fc6232]">Registrate</Link></p>
-                </form>
-            </div>
-            </div>
-        }
+            }
         </>
     )
 }
