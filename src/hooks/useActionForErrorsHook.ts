e7dfbd13a205581a45205a42f2a88ae1fe2fc0d7
mvblanco99 <manuelvalentin1999@gmail.com>
2024-04-29 21:42:36 -0400

    Implementamos hook para realizar acciones segun la respuesta de la api

---
+++ src/hooks/useActionForErrorsHook.ts
@@ -0,0 +1,39 @@
+import { useNavigate } from "react-router-dom";
+import { errorMessagesApi } from "../utils/errorMessagesApi";
+
+type Props = { 
+    status: number, 
+    statusText: string
+}
+
+export const useActionForErrorsHook = () => {
+    
+    const navigate  = useNavigate();
+
+    const executeActions = ({status, statusText}:Props) => {
+        if(status == 404){
+            alert('Ha ocurrido un error');
+        }
+
+        if(status == 403){
+            if(statusText === errorMessagesApi.actionUnauthorized){
+                alert(statusText);
+            }else if(statusText === errorMessagesApi.errorVerificationEmail){
+                //Redireccionar
+                navigate('/profile')
+            }
+        }
+        
+        if(status == 401){
+            navigate('/')
+        }
+        
+        if(status == 500){
+            alert('Estamos presentando problemas, por favor intente mas tarde');
+        }
+    }
+
+    return {
+        executeActions
+    }
+}
