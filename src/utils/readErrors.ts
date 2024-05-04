b7d2b17344c7eac39de0eb8b295bb220b779eaec
mvblanco99 <manuelvalentin1999@gmail.com>
2024-05-03 21:21:34 -0400

    Corregimos pequeño error de tipeo con respecto al retorno del  error 403

--- src/utils/readErrors.ts
+++ src/utils/readErrors.ts
@@ -2,6 +2,6 @@ export const readError = (error) => {
     if(error.code === 'ERR_CANCELED') console.log(error.message);
     if(error.code === 'ECONNABORTED') return { status: 500, statusText:'El servidor ha tardado mucho en responder'};
     if(error?.request.status === 422) return {status: 422, errors: error?.response?.data?.errors}; 
-    if(error?.request.status === 403) return {status: 403, statusTex: error?.response?.data?.message};
+    if(error?.request.status === 403) return {status: 403, statusText: error?.response?.data?.message};
     return { status: error?.request.status, statusText: error?.request.statusText };
 }
