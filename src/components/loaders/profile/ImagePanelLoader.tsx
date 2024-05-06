55f753d1f8c7380af63d4ea4b1a665e4ab169d3e
mvblanco99 <manuelvalentin1999@gmail.com>
2024-05-06 16:27:52 -0400

    Reestruramos loaders segun categoria

---
+++ src/components/loaders/profile/ImagePanelLoader.tsx
@@ -0,0 +1,19 @@
+import ImageLoader from "./ImageLoader"
+
+const ImagePanelLoader = () => {
+  return (
+    <div className="w-full grid grid-cols-3">
+        <ImageLoader/>
+        <ImageLoader/>
+        <ImageLoader/>
+        <ImageLoader/>
+        <ImageLoader/>
+        <ImageLoader/>
+        <ImageLoader/>
+        <ImageLoader/>
+        <ImageLoader/>
+    </div>
+  )
+}
+
+export default ImagePanelLoader
