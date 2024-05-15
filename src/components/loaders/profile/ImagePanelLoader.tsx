8dca313d1d7ef11faf05c3a84f79c6f2865076a9
mvblanco99 <manuelvalentin1999@gmail.com>
2024-05-14 21:33:08 -0400

    Agregamos distintos loaders para la seccion del profile

--- src/components/loaders/profile/ImagePanelLoader.tsx
+++ src/components/loaders/profile/ImagePanelLoader.tsx
@@ -1,17 +1,18 @@
 import ImageLoader from "./ImageLoader"
 
 const ImagePanelLoader = () => {
+  
   return (
     <div className="w-full grid grid-cols-3">
-        <ImageLoader/>
-        <ImageLoader/>
-        <ImageLoader/>
-        <ImageLoader/>
-        <ImageLoader/>
-        <ImageLoader/>
-        <ImageLoader/>
-        <ImageLoader/>
-        <ImageLoader/>
+      <ImageLoader/>
+      <ImageLoader/>
+      <ImageLoader/>
+      <ImageLoader/>
+      <ImageLoader/>
+      <ImageLoader/>
+      <ImageLoader/>
+      <ImageLoader/>
+      <ImageLoader/>
     </div>
   )
 }
