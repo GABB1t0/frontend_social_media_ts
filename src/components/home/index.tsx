6f8216c9328943c667f3bb0ea6f80279120695cf
mvblanco99 <manuelvalentin1999@gmail.com>
2024-05-19 19:28:07 -0400

    Comentamos el contentHome para solucionar bug

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,49 +1,27 @@
 import { Suspense, lazy } from "react";
-
-
 import Loader from "../loaders/redirections/LoaderForRedirections";
 import { useVerifyUser } from "../../hooks/useVerifyUser";
 import useReduxHook from "../../hooks/useReduxHook";
 import { RootState } from "../../app/store";
 import { useDispatchUser } from "../../hooks/useDispatchUser";
 
-
-
-
-
 const Header = lazy(() => import('../header/Header'));
 const ContentHome = lazy(() => import('./ContentHome'));
 
-
 const TemplateHome = () => {
 
-  const { myUseSelector, dispatch } = useReduxHook();
+  const { myUseSelector } = useReduxHook();
   const userLogged = myUseSelector((state:RootState) => state?.userLogged);
 
   useDispatchUser()
-
-  
   const {verified} = useVerifyUser();
   
-      
   return (
     <>
-    
-      
-      
-        <Header navBlock={false} />
-        <Suspense fallback={<div><Loader/></div>}>
-          
-          {(verified && userLogged.entities) &&
-          
-          <ContentHome/>}
-            
-        </Suspense>
-      
-    
-      
-      
-      
+      <Suspense>
+        <Header navBlock={false}/>
+        {/* {(verified && userLogged.entities) && <ContentHome/>}       */}
+      </Suspense>
     </>
   );
 }
